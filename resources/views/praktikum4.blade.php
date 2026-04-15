b93d9fdf6b7836d2e63c232671bf6e2546713686
faishal-nafis <muhammadfaishalnafis@gmail.com>
2026-04-15 16:51:26 +0700

    adding view nafis

--- resources/views/praktikum4.blade.php
+++ resources/views/praktikum4.blade.php
@@ -1,19 +1,38 @@
-<html>
-        <table>
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Nama</th>
-                    <th>Harga</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($praktikum4 as $item)
-                <tr>
-                    <td>{{ $item['id'] }}</td>
-                    <td>{{ $item['nama'] }}</td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Praktikum 4 & 5</title>
+    
+    <link rel="stylesheet" href="{{ asset('styles/style_nafis.css') }}">
+</head>
+<body>
+
+    <h1>Ini Judul Merah</h1>
+    <img src="{{ asset('images/f1_mercedes.jpg') }}" alt="Gambar 1">
+    <img src="{{ asset('images/f1_redbull.jpg') }}" alt="Gambar 2">
+
+    <br><hr><br>
+
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nama</th>
+                <th>Harga</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($praktikum4 as $item)
+            <tr>
+                <td>{{ $item['id'] }}</td>
+                <td>{{ $item['nama'] }}</td>
+                <td>{{ $item['harga'] ?? '-' }}</td> 
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+
+</body>
 </html>
