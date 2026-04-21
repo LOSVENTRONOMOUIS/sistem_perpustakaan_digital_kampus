4effc80d352d2324131e73332bd931568807badb
faishal-nafis <muhammadfaishalnafis@gmail.com>
2026-04-21 14:11:35 +0700

    update vite

--- resources/views/praktikum4.blade.php
+++ resources/views/praktikum4.blade.php
@@ -4,9 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Praktikum 4 & 5</title>
-    
-    
-    @vite(['styles/style_nafis.css', 'resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
 
